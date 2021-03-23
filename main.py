4321bb9138dbfaacc930232659da85da9d797cc8
JMcB <joel.mcbride1@live.com>
2021-03-23 12:20:18 +0000

    Make clip ripping concurrent
    Also add requirements. add to .gitignore, add notes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import json
+from decimal import Decimal
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 
 import ffmpeg
 
@@ -13,7 +15,6 @@ import ffmpeg
 
 videos_folder = Path('videos')
 audio_clips_folder = videos_folder / 'audio-clips'
-timestamps_file_path = audio_clips_folder / 'timestamps.json'
 
 
 def verify_timestamp_pairs(timestamps: list[tuple[float, float]], maximum: float = 0.0) -> bool:
@@ -27,36 +28,68 @@ def verify_timestamp_pairs(timestamps: list[tuple[float, float]], maximum: float
     return True
 
 
-def rip_audio_clips(video_path: Path, timestamps: list[tuple[float, float]]):
+# made this way to work with ThreadPoolExecutor
+class ClipRipper:
+    def __init__(self, video_path, clips_folder):
+        self.video_path = video_path
+        self.clips_folder = clips_folder
+
+    def rip_audio_clip(self, t: tuple[float, float]):
+        duration = Decimal(str(t[1])) - Decimal(str(t[0]))
+        clip_path = self.clips_folder / f'{t[0]}d{duration}.mp3'
+        stream = ffmpeg.input(str(self.video_path), ss=t[0])
+        stream = ffmpeg.output(stream, str(clip_path), t=duration)
+        ffmpeg.run(stream)
+
+
+def rip_all_audio_clips(video_path: Path, timestamps: list[tuple[float, float]]):
     specific_clips_folder = audio_clips_folder / video_path.name
     specific_clips_folder.mkdir(parents=True, exist_ok=True)
 
-    for t in timestamps:
-        duration = t[1] - t[0]
-        clip_path = specific_clips_folder / f'{t[0]}-{duration}.mp3'
-        stream = ffmpeg.input(str(video_path), ss=t[0])
-        stream.output(str(clip_path))
-        stream.run()
+    clip_ripper = ClipRipper(video_path, specific_clips_folder)
+    # one thread
+    # for t in timestamps:
+    #     clip_ripper.rip_audio_clip(t)
+
+    # multithread
+    with ThreadPoolExecutor() as threads:
+        threads.map(clip_ripper.rip_audio_clip, timestamps)
 
 
 def add_audio_to_video(video_path: Path, audio_path: Path, timestamp: float, duration: float):
+    # add the audio onto the video at the timestamp with ffmpeg
+    pass
+
+
+def fill_empty_audio(video_path: Path, timestamps: list[tuple[float, float]]):
+    # remove audio from video
+    # for any duration not covered by a timestamp, add the original audio back
+    # will need to make algorithm more advanced if adding overlapping audio support
     pass
 
 
 def main():
+    # if starts with http or https, use ydl to download video and subtitles
     video_name = input('Video name: ')
     video_path = videos_folder / video_name
+    if not video_path.is_file():
+        print("Video doesn't exist")
+        return False
 
     print('Checking for timestamps file')
-    with open(timestamps_file_path) as timestamps_file:
+    # todo: change to just .json
+    with open(video_path.with_suffix('.ko.json')) as timestamps_file:
         timestamps = json.load(timestamps_file)
         print('Loaded timestamps file')
+    # look for subtitle file by name
+    # look for subtitle file by extension
+    # look for subtitle file with subtitle on pypi
     if not verify_timestamp_pairs(timestamps):
         print('Invalid timestamps')
         return False
 
     print('Ripping audio clips')
-    rip_audio_clips(video_path, timestamps)
+    rip_all_audio_clips(video_path, timestamps)
 
 
 if __name__ == '__main__':
