c060d8e0ac639d7af37eae590c4684bf71ad4008
JMcB <joel.mcbride1@live.com>
2021-04-03 20:13:09 +0100

    Oops

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', 'r', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 setuptools.setup(
-    name='cmpc-timelapse',
+    name='kingsquit',
     version=kingsquit.__version__,
     author='JMcB',
     author_email='joel.mcbride1@live.com',
